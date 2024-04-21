babb0a35a529ff4b51f07f7033d8e9d11e0ff661
Kevin Steve Sathyanath <satke569@student.otago.ac.nz>
2024-04-21 23:47:07 +1200

    simple plot

---
+++ pyplotpractice.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = [1,2,3,4,5,6,7,8,9]
+y = [1,2,3,4,5,6,7,8,9]
+
+fig, ax = plt.subplots()
+ax.plot(x,y)
+
+ax.set(xlabel = 'x', ylabel = 'y', title = 'Simple plot')
+ax.grid()
+
+plt.show()
+
